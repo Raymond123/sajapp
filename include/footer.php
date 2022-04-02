ae04243eb99d2c04adc155fa3e225d01e2fdd487
eastonsmith <eastonsmith37@gmail.com>
2022-04-02 15:06:23 -0400

    added header and included header.php into index.php

---
+++ include/footer.php
@@ -0,0 +1,15 @@
+<div class="container">
+    <footer class="py-3 my-4">
+        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+        </ul>
+
+        <p class="text-center text-light">© 2022 Saj Co., sajapp</p>
+        <p class="text-center text-light">Easton, Sajeev </p>
+    </footer>
+</div>
+</body>
+</html>
+
+
+
+<?php
